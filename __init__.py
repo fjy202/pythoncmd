81445d6232314b5900032756f4bc46ed86752b2d
fjy202 <40914915+fjy202@users.noreply.github.com>
2019-01-13 20:45:51 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
-import os
-class pysh:
-    def __getatrribute__(attr):
-        class cmd:
+import os,sys
+class __main__:
+    def __getattr__(self,attr):
+        class _cmd:
             def __init__(self,command):
                 self.cmd=command
             def __call__(self,*args):
                 if self.cmd=='cd':
                     return os.chdir(*args[0])
-                return os.system(self.cmd+' '+' '.join(args))
-        return cmd(attr)
+                return os.system(self.cmd+' '+(' '.join(args)))
+            def __repr__(self):
+                return '<cmd object command='+self.cmd+'>'
+        return _cmd(attr)
+sys.modules['pythoncmd']=__main__()
+
